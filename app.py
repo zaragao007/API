1387f46d8d335bf0893fb41dba2e392f68f81351
zaragao007 <zaragao@gmail.com>
2025-09-03 14:49:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
 st.header('Jogando uma moeda')
+number_of_trials = st.slider('Número de tentativas?', 1, 1000, 10)
+start_button = st.button('Executar')
 
+if start_button:
+    st.write(f'Executando o experimento de {number_of_trials} tentativas.')
 st.write('Ainda não é um aplicativo funcional. Em construção.')
